f0e4a5abdf4677795e9f502307403d2176de8ef6
cc <173192386@qq.com>
2025-05-09 03:49:26 +0800

    init backend and do some yml changes

---
+++ xueBuTong-backend/src/main/java/com/cc/xuebutongbackend/XueBuTongBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cc.xuebutongbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XueBuTongBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XueBuTongBackendApplication.class, args);
+    }
+
+}
---
+++ xueBuTong-backend/src/test/java/com/cc/xuebutongbackend/XueBuTongBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cc.xuebutongbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XueBuTongBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
